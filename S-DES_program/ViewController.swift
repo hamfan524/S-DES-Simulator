45119c79741431eefb0be167796214d5986d5738
Choe DongHo <hamfan524@naver.com>
2021-11-21 03:17:20 +0900

    Initial Commit

---
+++ S-DES_program/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  S-DES_program
+//
+//  Created by 최동호 on 2021/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
